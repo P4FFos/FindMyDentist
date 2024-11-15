21c69129b6f5218c01e239fc087c343fcf3e4753
mykhailo <mykhailo@student.chalmers.se>
2024-11-16 00:12:17 +0100

    Issue#9 initialise client side

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,5 @@
 import { createApp } from 'vue'
-import { createBootstrap } from 'bootstrap-vue-next'
+import './style.css'
 import App from './App.vue'
-import router from './router'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
-import './assets/global.css'
-
-const app = createApp(App)
-app.use(createBootstrap())
-app.use(router)
-app.mount('#app')
+createApp(App).mount('#app')
