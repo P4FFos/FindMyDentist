9aafa257d8a6149e3ccd9bd4dcc8cf174acc0c00
mykhailo <mykhailo@student.chalmers.se>
2024-12-12 21:32:33 +0100

    Issue#17 implement responsive UI for the login and registration pages

--- client/src/views/LoginPage.vue
+++ client/src/views/LoginPage.vue
@@ -1,22 +1,23 @@
 <template>
-  <div class="login">
-    <h1>Login</h1>
-    <div>
-      <button @click="selectUserType('patient')">Patient</button>
-      <button @click="selectUserType('dentist')">Dentist</button>
+    <div class="d-flex flex-column align-items-center justify-content-center vh-100 text-center content">
+      <h1 class="title">Login</h1>
+      <p>Welcome back! Please login to your account</p>
+      <div class="user-type-selection">
+        <b-button @click="selectUserType('patient')" class="textButton">Patient</b-button>
+        <b-button @click="selectUserType('dentist')" class="textButton">Dentist</b-button>
+      </div>
+      <form @submit.prevent="login" class="mt-3 d-flex flex-column w-30">
+        <label for="email">Email:</label>
+        <input type="email" v-model="email" required/>
+
+        <label for="password">Password:</label>
+        <input type="password" v-model="password" required/>
+
+        <button class="button" type="submit">Login</button>
+      </form>
+      <p v-if="message">{{ message }}</p>
     </div>
-    <form @submit.prevent="login">
-      <label for="email">Email:</label>
-      <input type="email" v-model="email" required/>
-
-      <label for="password">Password:</label>
-      <input type="password" v-model="password" required/>
-
-      <button type="submit">Login</button>
-    </form>
-    <p v-if="message">{{ message }}</p>
-  </div>
-</template>
+  </template>
 
 <script>
 import mqtt from 'mqtt';
@@ -36,15 +37,14 @@ export default {
     // Set the user type
     selectUserType(type) {
       this.userType = type;
+      this.message = `You are loging in as a ${type}`;
     },
     // Set up the MQTT client
     setupMqttClient() {
       this.mqttClient = mqtt.connect('ws://localhost:8080');
       this.mqttClient.on('connect', () => {
-        console.log('MQTT connected');
         this.mqttClient.subscribe('patients/get/login/response');
         this.mqttClient.subscribe('dentists/get/login/response');
-        console.log('Subscribed to login topics');
       });
 
       this.mqttClient.on('message', (topic, message) => {
@@ -92,6 +92,32 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+    p {
+        color: #7E7E7E;
+    }
+    label {
+        align-self:flex-start;
+        font-weight: bold;
+    }
+    .user-type-selection {
+        color: black;
+    }
+    .button {
+        margin-top: 2rem;
+    }
+    .textButton:hover {
+        background-color: #dbdbd9;
+    }
+    .w-30 {
+        width: 28%;
+    }
+    @media (max-width: 767px) {
+        .title {
+            font-size: 3rem;
+        }
+        .w-30 {
+            width: 70%;
+        }
+    }
 </style>
--- client/src/views/StartPage.vue
+++ client/src/views/StartPage.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="d-flex flex-column align-items-center justify-content-center vh-100 text-center ">
-      <h1 class="fw-bold">Find My Dentist</h1>
+      <h1 class="title fw-bold">Find My Dentist</h1>
       <p class="text-muted">the best options for the best city</p>
       <div class="mt-3 d-flex flex-column">
         <button variant="primary" class="me-2" @click="goToRegister">Sign Up</button>
-        <label >Already have an account? <b-button variant="link" @click="goToLogin">Log In</b-button></label>
+        <label >Already have an account? <b-button variant="link" @click="goToLogin" class="login-button">Log In</b-button></label>
       </div>
     </div>
 </template>
@@ -30,4 +30,13 @@
 </script>
 
 <style>
+    .login-button {
+        color: #1888AE;
+    }
+
+    @media (max-width: 767px) {
+        .title {
+            font-size: 2.5rem;
+        }
+    }
 </style>
