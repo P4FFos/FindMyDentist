4b0588a615b71dd2adc3de6b2b07f3e8860413a1
Stefan Tram <stefantr@student.chalmers.se>
2025-01-01 20:33:10 +0000

    Revert "Issue#27 Test pipeline"
    This reverts commit 8025b80e0414fb1b989e9a802d338ab67f595d24

--- mqtt/mqtt-config.js
+++ mqtt/mqtt-config.js
@@ -1,12 +1,11 @@
 const mqtt = require("mqtt");
-require('dotenv').config();
 
 const protocol = 'mqtt';
 const host = 'localhost';
 const port = '1883';
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
 
-const connectUrl = process.env.CI_MQTT_URL || `${protocol}://${host}:${port}`;
+const connectUrl = `${protocol}://${host}:${port}`;
 
 const client = mqtt.connect(connectUrl, {
     clientId,
