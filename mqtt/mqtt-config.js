8025b80e0414fb1b989e9a802d338ab67f595d24
Stefan Tram <stefantr@student.chalmers.se>
2025-01-01 21:15:55 +0100

    Issue#27 Test pipeline

--- mqtt/mqtt-config.js
+++ mqtt/mqtt-config.js
@@ -1,11 +1,12 @@
 const mqtt = require("mqtt");
+require('dotenv').config();
 
 const protocol = 'mqtt';
 const host = 'localhost';
 const port = '1883';
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
 
-const connectUrl = `${protocol}://${host}:${port}`;
+const connectUrl = process.env.CI_MQTT_URL || `${protocol}://${host}:${port}`;
 
 const client = mqtt.connect(connectUrl, {
     clientId,
