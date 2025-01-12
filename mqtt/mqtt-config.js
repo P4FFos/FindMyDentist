85c692067a19f37ce686667d6839e188bb7d826f
mykhailo <mykhailo@student.chalmers.se>
2025-01-12 12:57:21 +0100

    test pipeline

--- mqtt/mqtt-config.js
+++ mqtt/mqtt-config.js
@@ -5,7 +5,7 @@ const host = 'localhost';
 const port = '8080';
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
 
-const connectUrl = `${protocol}://${host}:${port}`;
+const connectUrl = process.env.MQTT_URL || `${protocol}://${host}:${port}`;
 
 const client = mqtt.connect(connectUrl, {
     clientId,
